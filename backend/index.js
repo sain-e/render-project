df2505e8f6521c28749c45c5119fd63b30cffb09
sain-e <lyusaine@gmail.com>
2025-06-23 17:46:16 +0200

    Update dotenv package

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ALLOWED_URL = process.env.ALLOWED_URL;
