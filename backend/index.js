8a636c1725a6512613b9ace1ef480308a1ff4a6f
sain-e <lyusaine@gmail.com>
2025-06-23 16:54:04 +0200

    Change ENVs and adding user

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,18 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const ALLOWED_URL = process.env.ALLOWED_URL;
 
 // Middleware
-app.use(cors({ origin: '*' }));  // specify ruta aqui
+app.use(cors({ origin: ALLOWED_URL }));  // specify ruta aqui
 app.use(express.json());
 
 // Dummy users data
 const users = [
     { id: 1, name: "Alice", email: "alice@example.com" },
     { id: 2, name: "Bob", email: "bob@example.com" },
-    { id: 3, name: "Charlie", email: "charlie@example.com" }
+    { id: 3, name: "Charlie", email: "charlie@example.com" },
+    { id: 4, name: "Alex", email: "alex@example.com" }
 ];
 
 // Users route
